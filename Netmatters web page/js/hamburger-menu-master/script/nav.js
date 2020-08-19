310ae70da207794d37419c9550a1598078e8d3ab
Dermot Wilson <dermot.wilson@netmatters-scs.com>
2020-08-19 18:19:48 +0100

    CSS for hamburger side menu

--- Netmatters web page/js/hamburger-menu-master/script/nav.js
+++ Netmatters web page/js/hamburger-menu-master/script/nav.js
@@ -3,6 +3,9 @@
 	let hamburger = {
 		nav: document.querySelector('#nav'),
 		navToggle: document.querySelector('.nav-toggle'),
+		body: document.querySelector('body'),
+		bodyA: document.querySelector('body:after'),
+		contact: document.querySelector('.contact'),
 
 		initialize() {
 			this.navToggle.addEventListener('click', () => { this.toggle(); });
@@ -11,6 +14,11 @@
 		toggle() {
 			this.navToggle.classList.toggle('expanded');
 			this.nav.classList.toggle('expanded');
+			this.body.classList.toggle('expanded');
+			this.body.classList.toggle('noScroll');
+			this.body.classList.toggle('nav-toggle');
+			this.contact.classList.toggle('nav-toggle');
+			// this.bodyA.classList.toggle('expanded');
 		},
 	};
 
