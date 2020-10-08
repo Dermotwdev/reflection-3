52971b1c4b1a8599839c01922d75e0b492680180
Dermot Wilson <dermot.wilson@netmatters-scs.com>
2020-10-08 14:31:44 +0100

    Fixed header and hamburger menu issues

--- Netmatters web page/js/hamburger-menu-master/script/nav.js
+++ Netmatters web page/js/hamburger-menu-master/script/nav.js
@@ -1,93 +1,109 @@
 // **** Code written by me ****
-(function () {
-let hamburger = {
-	nav: document.querySelector('#nav'),
-	navToggle: document.querySelector('.nav-toggle'),
-	blocker: document.querySelector('#blocker'),
-   contact: document.querySelector('.contact'),
-   page: document.querySelector('#page-content'),
-
-		initialize() {
-		//this.navToggle.addEventListener('click', () => { this.toggle(); });
-		for (i=0; i<2; i++) {
-			document.querySelectorAll('.nav-toggle')[i].addEventListener('click', () => {this.toggle();})
-			}
-		},
-
-		toggle() {
-      console.log('toggled')
-		// Set page style to expanded
-		for (i=0; i<2; i++) {
-			document.querySelectorAll('.nav-toggle')[i].classList.toggle('expanded');
-		}
-		this.nav.classList.toggle('expanded');
-      this.page.classList.toggle('expanded');
-      this.blocker.classList.toggle('expanded');
-      // Allow clicking anywhere on 
-      if(this.blocker.classList.contains('expanded')) {
-        this.blocker.addEventListener('click', togglEvent);
-      } else {
-        this.removeEvent();
-      }
-    },
-    removeEvent() {
-      console.log('Event removed')
-      this.blocker.removeEventListener('click', togglEvent);
-    }
-	};
-
-  hamburger.initialize();
-
-  const togglEvent = function toggleEvent() {
-    hamburger.toggle();
-  }
-})();
-
-
-// **** Babel Code ****
-
-
-
-// "use strict";
-
 // (function () {
-//   var hamburger = {
-//     nav: document.querySelector('#nav'),
-//     navToggle: document.querySelector('.nav-toggle'),
-//     blocker: document.querySelector('#blocker'),
-//     contact: document.querySelector('.contact'),
-//     body: document.querySelector('body'),
-//     initialize: function initialize() {
-//       var _this = this;
+// let hamburger = {
+// 	nav: document.querySelector('#nav'),
+// 	navToggle: document.querySelector('.nav-toggle'),
+// 	blocker: document.querySelector('#blocker'),
+//   contact: document.querySelector('.contact'),
+//   page: document.querySelector('#page-content'),
 
-//       this.navToggle.addEventListener('click', function () {
-//         _this.toggle();
-//       });
-//     },
-//     toggle: function toggle() {
-//       console.log('toggled'); // Set page style to expanded
+// 		initialize() {
+// 		//this.navToggle.addEventListener('click', () => { this.toggle(); });
+// 		for (let i=0; i<2; i++) {
+// 			document.querySelectorAll('.nav-toggle')[i].addEventListener('click', () => {this.toggle();})
+// 			}
+// 		},
 
-//       this.navToggle.classList.toggle('expanded');
+// 		toggle() {
+//       console.log('toggled')
+//       // Set page style to expanded
+//       for (let i=0; i<2; i++) {
+//         document.querySelectorAll('.nav-toggle')[i].classList.toggle('expanded');
+//       }
+//       if (this.nav.classList.contains('hide-sidebar')) {
+//         this.nav.classList.toggle('hide-sidebar')
+//       } else {
+//         setTimeout(function() {
+//           this.nav.classList.toggle('hide-sidebar');
+//         }, 600)
+//       }
 //       this.nav.classList.toggle('expanded');
-//       this.body.classList.toggle('expanded');
-//       this.blocker.classList.toggle('expanded'); // Set rest of page to not scroll
-
-//       this.body.classList.toggle('noScroll'); // Allow clicking anywhere on 
-
-//       if (this.blocker.classList.contains('expanded')) {
+//       this.page.classList.toggle('expanded');
+//       this.blocker.classList.toggle('expanded');
+//       // Allow clicking anywhere on 
+//       if(this.blocker.classList.contains('expanded')) {
 //         this.blocker.addEventListener('click', togglEvent);
 //       } else {
 //         this.removeEvent();
 //       }
 //     },
-//     removeEvent: function removeEvent() {
-//       console.log('Event removed');
+//     removeEvent() {
+//       console.log('Event removed')
 //       this.blocker.removeEventListener('click', togglEvent);
 //     }
-//   };
+// 	};
+
 //   hamburger.initialize();
 
-//   var togglEvent = function toggleEvent() {
+//   const togglEvent = function toggleEvent() {
 //     hamburger.toggle();
-//   };
+//   }
 // })();
+
+
+// **** Babel Code ****
+"use strict";
+
+(function () {
+  var hamburger = {
+    nav: document.querySelector('#nav'),
+    navToggle: document.querySelector('.nav-toggle'),
+    blocker: document.querySelector('#blocker'),
+    contact: document.querySelector('.contact'),
+    page: document.querySelector('#page-content'),
+    initialize: function initialize() {
+      var _this = this;
+
+      //this.navToggle.addEventListener('click', () => { this.toggle(); });
+      for (var i = 0; i < 2; i++) {
+        document.querySelectorAll('.nav-toggle')[i].addEventListener('click', function () {
+          _this.toggle();
+        });
+      }
+    },
+    toggle: function toggle() {
+      console.log('toggled'); // Set page style to expanded
+
+      for (var i = 0; i < 2; i++) {
+        document.querySelectorAll('.nav-toggle')[i].classList.toggle('expanded');
+      }
+
+      if (this.nav.classList.contains('hide-sidebar')) {
+        this.nav.classList.toggle('hide-sidebar');
+      } else {
+        setTimeout(function () {
+          this.nav.classList.toggle('hide-sidebar');
+        }, 600);
+      }
+
+      this.nav.classList.toggle('expanded');
+      this.page.classList.toggle('expanded');
+      this.blocker.classList.toggle('expanded'); // Allow clicking anywhere on 
+
+      if (this.blocker.classList.contains('expanded')) {
+        this.blocker.addEventListener('click', togglEvent);
+      } else {
+        this.removeEvent();
+      }
+    },
+    removeEvent: function removeEvent() {
+      console.log('Event removed');
+      this.blocker.removeEventListener('click', togglEvent);
+    }
+  };
+  hamburger.initialize();
+
+  var togglEvent = function toggleEvent() {
+    hamburger.toggle();
+  };
+})();
